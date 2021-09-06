bd17ab84fbddcd758fbcbc7878ccf5d67c822edd
DESKTOP-81UOKVS\Daniel <dmatschek@gmail.com>
2021-09-06 18:43:56 +0200

    added defaultsite

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
+    path("defaultsite", hello.views.index, name="index"),
 ]
